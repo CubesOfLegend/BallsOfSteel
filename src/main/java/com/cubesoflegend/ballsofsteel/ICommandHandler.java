829fde1bcaf7e3b89d79d51bc5821b380ba4f73b
aturcey <aturcey@users.noreply.github.com>
2015-09-28 22:00:33 +0200

    Implementation commande /removespawn <arena> <color>

--- src/main/java/com/cubesoflegend/ballsofsteel/ICommandHandler.java
+++ src/main/java/com/cubesoflegend/ballsofsteel/ICommandHandler.java
@@ -10,6 +10,7 @@ import com.comze_instancelabs.minigamesapi.Arena;
 import com.comze_instancelabs.minigamesapi.MinigamesAPI;
 import com.comze_instancelabs.minigamesapi.PluginInstance;
 import com.comze_instancelabs.minigamesapi.commands.CommandHandler;
+import com.comze_instancelabs.minigamesapi.config.ArenasConfig;
 import com.comze_instancelabs.minigamesapi.util.Util;
 import com.cubesoflegend.ballsofsteel.utils.ColorUtils;
 
@@ -65,6 +66,44 @@ public class ICommandHandler extends CommandHandler {
         } else {
             sender.sendMessage(ChatColor.DARK_GRAY + "[" + ChatColor.RED + "-" + ChatColor.DARK_GRAY + "]"
                     + ChatColor.GRAY + " Usage: " + cmd + " " + action + " <arena>");
+        }
+        return true;
+    }
+    
+    @Override
+    public boolean removeSpawn(PluginInstance pli, CommandSender sender, String[] args, String uber_permission, String cmd, String action, JavaPlugin plugin, Player p) {
+        if (!sender.hasPermission(uber_permission + ".setup")) {
+            sender.sendMessage(pli.getMessagesConfig().no_perm);
+            return true;
+        }
+        if (args.length > 2) {
+            String team = args[2].toLowerCase();
+            if (!ColorUtils.bimapColorChatColor.containsKey(team)) {
+                sender.sendMessage(ChatColor.DARK_GRAY + "[" + ChatColor.RED + "-" + ChatColor.DARK_GRAY + "]"
+                        + ChatColor.GRAY + " Usage: " + cmd + " " + action + " <arena> <team>");
+                sender.sendMessage(
+                        ChatColor.RED + "Teams possible: Bleu, Rouge, Jaune, Vert, Rose, Violet, Orange, Noir, Blanc");
+                return true;
+            }
+            else
+            {
+                ArenasConfig config = MinigamesAPI.getAPI().getPluginInstance(plugin).getArenasConfig();
+                boolean ret = false;
+                String path = "arenas." + args[1] + ".spawns.spawn" + team;
+                if (config.getConfig().isSet(path)) {
+                    config.getConfig().set(path, null);
+                    config.saveConfig();
+                    sender.sendMessage(pli.getMessagesConfig().successfully_removed.replaceAll("<component>", "spawn " + args[2]));
+                    return true;
+                }
+                else
+                {
+                    sender.sendMessage(pli.getMessagesConfig().failed_removing_component.replaceAll("<component>", "spawn " + args[2]).replaceAll("<cause>", "Possibly the provided spawn couldn't be found: " + args[2]));
+                    return true;
+                }
+            }
+        } else {
+            sender.sendMessage(ChatColor.DARK_GRAY + "[" + ChatColor.RED + "-" + ChatColor.DARK_GRAY + "]" + ChatColor.GRAY + " Usage: " + cmd + " " + action + " <arena> <team>");
         }
         return true;
     }
